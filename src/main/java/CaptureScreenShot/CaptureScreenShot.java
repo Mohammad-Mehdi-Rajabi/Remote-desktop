d65abae52edccc261a96e7a16198a62f9295748f
Mohammad-Mehdi-Rajabi <mohammadmehdirajabi9@gmail.com>
2022-03-10 02:59:32 +0330

    end of this branch

--- src/main/java/CaptureScreenShot/CaptureScreenShot.java
+++ src/main/java/CaptureScreenShot/CaptureScreenShot.java
@@ -1,9 +1,75 @@
-package Capture;
+package CaptureScreenShot;
 
+
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
- * this
+ * this class take screenShot
  */
-public class Capture {
+public class CaptureScreenShot {
+    private BufferedImage bufferedImage;
+    private Rectangle rectangle;
+    private Robot robot;
+
+    public CaptureScreenShot(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public CaptureScreenShot() {
+    }
+
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public BufferedImage getBufferedImage() {
+        return bufferedImage;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public Robot getRobot() {
+        return robot;
+    }
+
+
+    /**
+     * external thread safe method to take screenShot
+     *
+     * @return BufferedImage
+     */
+    public synchronized BufferedImage takeScreenShot() throws AWTException, NullPointerException {
+        if (rectangle != null) {
+            robot = new Robot();
+            this.bufferedImage = robot.createScreenCapture(rectangle);
+        } else {
+            throw new NullPointerException("Rectangle is null");
+        }
+        return this.bufferedImage;
+    }
+
+
+    /**
+     * static thread safe method to take screenShot using injected rectangle
+     * this method doesn't use or init class fields
+     *
+     * @param rectangle rectangle to screenShot size
+     * @return BufferedImage
+     * @throws AWTException         if can't create Robot
+     * @throws NullPointerException if rectangle was null
+     */
+    public synchronized static BufferedImage takeScreenShot(Rectangle rectangle) throws AWTException, NullPointerException {
+        if (rectangle != null) {
+            return new Robot().createScreenCapture(rectangle);
+        } else {
+            throw new NullPointerException("Rectangle is null");
+        }
+    }
+
 
 }
--- src/test/java/CaptureScreenShot/CaptureScreenShotTest.java
+++ src/test/java/CaptureScreenShot/CaptureScreenShotTest.java
@@ -1,5 +1,73 @@
+package CaptureScreenShot;
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaptureScreenShotTest {
+    @Test
+    void takeScreenShotTest() {
+        // create directory if not exists
+        File directory = new File("test_pic");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        // delete all files in test_pic directory
+        Util.Util.deleteFiles(new File("test_pic"));
 
+        CaptureScreenShot captureScreenShot =
+                new CaptureScreenShot(
+                        new Rectangle(
+                                Toolkit.getDefaultToolkit().getScreenSize()
+                        )
+                );
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = captureScreenShot.takeScreenShot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        try {
+            ImageIO.write(bufferedImage, "jpg", new File("test_pic/takeScreenShotTest_Result.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(Files.exists(Paths.get("test_pic/takeScreenShotTest_Result.jpg")));
+    }
+
+    @Test
+    void takeScreenShotTest_static() {
+        // create directory if not exists
+        File directory = new File("test_pic");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        // delete all files in test_pic directory
+        Util.Util.deleteFiles(new File("test_pic"));
+        try {
+            BufferedImage bufferedImage = CaptureScreenShot.takeScreenShot(
+                    new Rectangle(
+                            Toolkit.getDefaultToolkit().getScreenSize()
+                    )
+            );
+            ImageIO.write(bufferedImage, "jpg", new File("test_pic/takeScreenShotTest_static_Result.jpg"));
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(Files.exists(Paths.get("test_pic/takeScreenShotTest_static_Result.jpg")));
+    }
 }
