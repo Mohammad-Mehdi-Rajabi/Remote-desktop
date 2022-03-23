9491ae1f55a391f240bb5521728ad4b6a42a4286
Mohammad-Mehdi-Rajabi <mohammadmehdirajabi9@gmail.com>
2022-03-23 21:24:50 +0430

    add getSystemIP method to Util class

--- src/main/java/Util/Util.java
+++ src/main/java/Util/Util.java
@@ -1,6 +1,10 @@
 package Util;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class Util {
     /**
@@ -16,5 +20,30 @@ public class Util {
                 deleteFiles(file);
             }
         }
+    }
+
+    /**
+     * static method to return system IP
+     * @return IP
+     */
+    public static String getSystemIP(){
+        String ip=null;
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                InetAddress addr = addresses.nextElement();
+                ip = addr.getHostAddress();
+                return ip;
+
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+        return ip;
     }
 }
