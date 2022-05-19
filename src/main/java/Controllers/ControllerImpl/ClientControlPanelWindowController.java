361e229035829b4dfbb5a79eaacd4a1636e341d7
Mohammad-Mehdi-Rajabi <mohammadmehdirajabi9@gmail.com>
2022-05-19 23:51:59 +0430

    commit ClientControlPanelWindowController

---
+++ src/main/java/Controllers/ControllerImpl/ClientControlPanelWindowController.java
@@ -0,0 +1,87 @@
+package Controllers.ControllerImpl;
+
+import Controllers.Controller;
+import Core.KeyBoard.KeyBoard.Keyboard;
+import Core.Manager.Client.ManagedClient;
+import Core.Mouse.Mouse.Mouse;
+import Core.ScreenShot.Runnable.CaptureScreenShotWithMouseRunnable;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientControlPanelWindowController implements Initializable, Controller {
+    public static Thread screenThread;
+    public static Thread mouseThread;
+    public static Thread keyBoardThread;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void onActionStopBtn(ActionEvent actionEvent) {
+        screenThread.stop();
+        mouseThread.stop();
+        keyBoardThread.stop();
+    }
+
+    public void onActionStartBtn(ActionEvent actionEvent) {
+        CaptureScreenShotWithMouseRunnable captureScreenShotWithMouseRunnable =
+                new CaptureScreenShotWithMouseRunnable();
+        captureScreenShotWithMouseRunnable.setOutputStream(ManagedClient.getIp(),
+                ManagedClient.getServers().get("screenServer").getPort());
+        screenThread = new Thread(captureScreenShotWithMouseRunnable);
+        screenThread.start();
+        mouseThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket mouseSocket = new Socket(ManagedClient.getIp(),
+                            ManagedClient.getServers().get("mouseServer").getPort());
+                    ObjectInputStream objectInputStream =
+                            new ObjectInputStream(mouseSocket.getInputStream());
+                    while (true) {
+                        Object o = objectInputStream.readObject();
+                        ((Mouse) o).run(new Robot());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mouseThread.start();
+        keyBoardThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket keyBoardSocket = new Socket(ManagedClient.getIp(),
+                            ManagedClient.getServers().get("keyBoardServer").getPort());
+                    ObjectInputStream objectInputStream =
+                            new ObjectInputStream(keyBoardSocket.getInputStream());
+                    while (true) {
+                        Object o = objectInputStream.readObject();
+                        ((Keyboard) o).run(new Robot());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        keyBoardThread.start();
+    }
+
+
+}
